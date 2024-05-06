7c65fdfbbf55d032625f86c35be3ee8b35758c3e
WillBearsch <william.bearsch@gmail.com>
2024-05-05 23:27:39 -0400

    Landscape component, camera and environment maps

--- 471-Project/src/App.jsx
+++ 471-Project/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { PerspectiveCamera, Environment, OrbitControls } from "@react-three/drei";
+import { SphereEnv } from "./SphereEnv";
+import { Landscape } from "./Landscape";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SphereEnv />
+      <Environment background={false} files={"assets/textures/envmap.hdr"} />
+
+      <PerspectiveCamera makeDefault position={[0, 10, 10]} />
+      <OrbitControls target={[0, 0, 0]}/>
+
+      <Landscape />  
+
+      <directionalLight
+        castShadow
+        color={"#f3d29a"}
+        intensity={2}
+        position={[10, 5, 4]}
+        shadow-bias={-0.0005}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-camera-near={0.01}
+        shadow-camera-far={20}
+        shadow-camera-top={6}
+        shadow-camera-bottom={-6}
+        shadow-camera-left={-6.2}
+        shadow-camera-right={6.4}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
